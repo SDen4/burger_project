e7bc7598b60c11afd66ac2485046676d120e0111
SDen4 <asw325@yandex.ru>
2019-11-20 00:05:55 +0500

    resolving problems with structure + nav + links etc.

--- config.rb
+++ config.rb
@@ -3,9 +3,9 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "img"
+css_dir = "./css/stylesheets"
+sass_dir = "./css/sass"
+images_dir = "./img"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
