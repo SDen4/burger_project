c8b9128cd432c0eaa981c051e17be966392f1f5f
SDen4 <asw325@yandex.ru>
2019-12-08 22:53:56 +0500

    hw3 final (rebuild js code of full screen menu by adding class) + fixed small bugs + 3 different bg for the page Best

--- js/full-screen-menu.js
+++ js/full-screen-menu.js
@@ -1,11 +1,14 @@
 const open = document.querySelector(".gamburger-menu__link");
 const close = document.querySelector(".gamburger-menu__close");
 const content = document.querySelector(".gamburger-menu__content");
+const bodyFixed = document.querySelector('body');
 
 open.addEventListener('click', function () {
-    content.style.display = 'flex';
+    content.classList.add('gamburger-menu__content_active');
+    bodyFixed.classList.add('body__fixed');
 });
 
 close.addEventListener('click', function () {
-    content.style.display = 'none';
+    content.classList.remove('gamburger-menu__content_active');
+    bodyFixed.classList.remove('body__fixed');
 });
