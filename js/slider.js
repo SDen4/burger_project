73b0e6820bc5564416dbfcf7447ccaae6917f8aa
SDen4 <asw325@yandex.ru>
2019-12-11 23:18:50 +0500

    js slider

---
+++ js/slider.js
@@ -0,0 +1,23 @@
+const left = document.querySelector('.slider__left'),
+      right = document.querySelector('.slider__right'),
+      items = document.querySelector('.slider__items'),
+      contWidth = document.querySelector('.slider__contant');
+
+
+right.addEventListener("click", function fn1() {
+    event.preventDefault();
+	let moveRight = rightCurr = parseInt(getComputedStyle(items).right) + parseInt(getComputedStyle(contWidth).width);
+    if (moveRight < parseInt(getComputedStyle(contWidth).width)*5) {
+        items.style.right = moveRight + 'px';
+        items.style.left = -moveRight + 'px';
+    };
+});
+
+left.addEventListener("click", function() {
+    event.preventDefault();
+	let moveLeft = parseInt(getComputedStyle(items).left) + parseInt(getComputedStyle(contWidth).width);
+    if (moveLeft < parseInt(getComputedStyle(contWidth).width)) {
+        items.style.left = moveLeft + 'px';
+        items.style.right = -moveLeft + 'px';
+    };
+});
