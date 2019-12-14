1f74140e142adf85d1d7c33953bd7e9051cccba6
SDen4 <asw325@yandex.ru>
2019-12-14 23:41:51 +0500

    sending for to server, links in full screen menu, links in header, small bugs

--- js/full-screen-menu.js
+++ js/full-screen-menu.js
@@ -2,6 +2,7 @@ const open = document.querySelector(".gamburger-menu__link");
 const close = document.querySelector(".gamburger-menu__close");
 const content = document.querySelector(".gamburger-menu__content");
 const bodyFixed = document.querySelector('body');
+const navLink = document.getElementsByClassName('navigation__link');
 
 open.addEventListener('click', function () {
     content.classList.add('gamburger-menu__content_active');
@@ -12,3 +13,15 @@ close.addEventListener('click', function () {
     content.classList.remove('gamburger-menu__content_active');
     bodyFixed.classList.remove('body__fixed');
 });
+
+content.addEventListener('click', function(e) {
+    if (e.target == content) {
+        close.click();
+    };
+});
+
+for (let i = 0; i<navLink.length; i++) {
+    navLink[i].addEventListener('click', function() {
+        close.click();
+    });
+};
