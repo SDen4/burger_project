3d9b2463b5aa6896e92666b0e6fa652b6959e311
SDen4 <asw325@yandex.ru>
2019-12-22 23:21:04 +0500

    fixed bud in stop scroll in fullscreen menu

--- js/full-screen-menu.js
+++ js/full-screen-menu.js
@@ -5,12 +5,14 @@ const open = document.querySelector(".gamburger-menu__link"),
       navLink = document.getElementsByClassName('navigation__link');
 
 open.addEventListener('click', function () {
+    flagScroll = true;
     content.classList.add('gamburger-menu__content_active');
     open.style.display = 'none';
     bodyFixed.classList.add('body__fixed');
 });
 
 close.addEventListener('click', function () {
+    flagScroll = false;
     content.classList.remove('gamburger-menu__content_active');
     open.style.display = 'block';
     bodyFixed.classList.remove('body__fixed');
