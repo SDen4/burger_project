80738b099a5616a77f202b56ae3cfc05f6dd04f6
SDen4 <asw325@yandex.ru>
2019-12-23 23:38:54 +0500

    fixed bugs with full screen menu and feedback modal

--- js/full-screen-menu.js
+++ js/full-screen-menu.js
@@ -1,21 +1,18 @@
 const open = document.querySelector(".gamburger-menu__link"),
       close = document.querySelector(".gamburger-menu__close"),
       content = document.querySelector(".gamburger-menu__content"),
-      bodyFixed = document.querySelector('body'),
       navLink = document.getElementsByClassName('navigation__link');
 
 open.addEventListener('click', function () {
     flagScroll = true;
     content.classList.add('gamburger-menu__content_active');
     open.style.display = 'none';
-    bodyFixed.classList.add('body__fixed');
 });
 
 close.addEventListener('click', function () {
     flagScroll = false;
     content.classList.remove('gamburger-menu__content_active');
     open.style.display = 'block';
-    bodyFixed.classList.remove('body__fixed');
 });
 
 content.addEventListener('click', function(e) {
