065e680ccac3253b62ba5cdac047707d7f2fa397
SDen4 <asw325@yandex.ru>
2019-12-26 23:16:15 +0500

    bug in icon of full screen menu

--- src/scripts/full-screen-menu.js
+++ src/scripts/full-screen-menu.js
@@ -3,17 +3,20 @@ close = document.querySelector(".gamburger-menu__close"),
 content = document.querySelector(".gamburger-menu__content"),
 navLinkG = document.getElementsByClassName('navigation__link_gamburger'); //navigation__link
 
+
 open.addEventListener('click', function () {
     flagScroll = true;
     content.classList.add('gamburger-menu__content_active');
-    open.style.display = 'none';
+    open.style.left = '-9990px';
+    //open.style.display = 'none';
     //open.classList.remove('gamburger-menu__link_active');
 });
 
 close.addEventListener('click', function () {
     flagScroll = false;
     content.classList.remove('gamburger-menu__content_active');
-    open.style.display = 'block';
+    open.style.left = '92%';
+    //open.style.display = 'block';
     //open.classList.add('gamburger-menu__link_active');
 });
 
